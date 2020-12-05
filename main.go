e16055e8a10b7edab1cd60951ba7579b8c8be654
CreeperExplosion <GhibranResearch@gmail.com>
2020-12-05 18:29:52 +0100

    4 threaded

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	}
 
 	for _, num := range nums {
-		fmt.Println(doFactorial(int64(num)) + "\n")
+		fmt.Println(fmt.Sprintf("%d! = %s \n", num, doFactorial(int64(num))))
 	}
 }
 
@@ -36,23 +36,41 @@ func doFactorial(num int64) string {
 
 	a := big.NewInt(1)
 	b := big.NewInt(1)
+	c := big.NewInt(1)
+	d := big.NewInt(1)
+
+	facRange := int64(num / 4)
 
 	wg.Add(1)
 	go func() {
-		a.MulRange(1, num/2)
+		a.MulRange(1, facRange)
 
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		b.MulRange((num/2)+1, num)
+		b.MulRange(facRange+1, facRange*2)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		c.MulRange((facRange*2)+1, facRange*3)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		d.MulRange((facRange*3)+1, num)
 		wg.Done()
 	}()
 
 	wg.Wait()
 	result := big.NewInt(1)
 	result.Mul(a, b)
+	result.Mul(result, c)
+	result.Mul(result, d)
 
-	return fmt.Sprintf("%d! = %s ", num, result.String())
+	return result.String()
 }
